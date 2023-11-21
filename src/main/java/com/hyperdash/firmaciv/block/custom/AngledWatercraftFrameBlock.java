48d5dad042ef71f9062c6fcce86dd0cf26a682b7
Alexis Stevens <hyperdashpony@gmail.com>
2023-11-21 09:12:44 -0800

    loot tables

--- src/main/java/com/hyperdash/firmaciv/block/custom/AngledWatercraftFrameBlock.java
+++ src/main/java/com/hyperdash/firmaciv/block/custom/AngledWatercraftFrameBlock.java
@@ -1,11 +1,8 @@
 package com.hyperdash.firmaciv.block.custom;
 
 import com.hyperdash.firmaciv.block.FirmacivBlocks;
-import com.hyperdash.firmaciv.block.OarlockBlock;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
 public class AngledWatercraftFrameBlock extends SquaredAngleBlock{
