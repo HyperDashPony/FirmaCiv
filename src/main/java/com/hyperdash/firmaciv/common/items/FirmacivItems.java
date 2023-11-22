809c86748363332dd65abdd6adbaf6770e66b224
Alexis Stevens <hyperdashpony@gmail.com>
2023-11-22 08:33:55 -0800

    new recipes for navigation tools

--- src/main/java/com/hyperdash/firmaciv/common/items/FirmacivItems.java
+++ src/main/java/com/hyperdash/firmaciv/common/items/FirmacivItems.java
@@ -15,15 +15,23 @@ public class FirmacivItems {
     public static final RegistryObject<Item> SEXTANT = ITEMS.register("sextant",
             () -> new SextantItem(new Item.Properties().stacksTo(1)));
 
+    public static final RegistryObject<Item> UNFINISHED_SEXTANT = ITEMS.register("unfinished_sextant",
+            () -> new SextantItem(new Item.Properties().stacksTo(1)));
+
     public static final RegistryObject<Item> NAV_CLOCK = ITEMS.register("nav_clock",
             () -> new ClockItem(new Item.Properties().stacksTo(1)));
 
+    public static final RegistryObject<Item> UNFINISHED_NAV_CLOCK = ITEMS.register("unfinished_nav_clock",
+            () -> new Item(new Item.Properties().stacksTo(64)));
     public static final RegistryObject<Item> BAROMETER = ITEMS.register("barometer",
             () -> new BarometerItem(new Item.Properties().stacksTo(1)));
 
+    public static final RegistryObject<Item> UNFINISHED_BAROMETER = ITEMS.register("unfinished_barometer",
+            () -> new BarometerItem(new Item.Properties().stacksTo(1)));
+
+
     public static final RegistryObject<Item> LARGE_WATERPROOF_HIDE = ITEMS.register("large_waterproof_hide",
             () -> new Item(new Item.Properties()));
-
     public static final RegistryObject<Item> KAYAK_PADDLE = ITEMS.register("kayak_paddle",
             () -> new Item(new Item.Properties().stacksTo(1)));
 
@@ -35,6 +43,8 @@ public class FirmacivItems {
 
     public static final RegistryObject<Item> COPPER_BOLT = ITEMS.register("copper_bolt",
             () -> new Item(new Item.Properties().stacksTo(64)));
+
+
 
     public static final RegistryObject<Item> KAYAK = ITEMS.register("kayak",
             () -> new KayakItem(new Item.Properties().stacksTo(1)));
