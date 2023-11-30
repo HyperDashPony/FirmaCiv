5c218d0211e8e85c39d01a8b8c70d981d41d5d35
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-11-29 22:37:51 -0800

    Block Entity rework. Using an ItemHandler now as it's convenient. We however don't expose it as a capability

--- src/main/java/com/hyperdash/firmaciv/common/blockentity/WatercraftFrameBlockEntity.java
+++ src/main/java/com/hyperdash/firmaciv/common/blockentity/WatercraftFrameBlockEntity.java
@@ -1,35 +1,50 @@
 package com.hyperdash.firmaciv.common.blockentity;
 
+import com.hyperdash.firmaciv.common.item.FirmacivItems;
+import com.hyperdash.firmaciv.util.FirmacivTags;
 import net.dries007.tfc.util.Helpers;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class WatercraftFrameBlockEntity extends BlockEntity {
-    private ItemStack planks = ItemStack.EMPTY;
-    private ItemStack bolts = ItemStack.EMPTY;
+
+    public static final int plankSlot = 0;
+    public static final int boltSlot = 1;
+    /**
+     * ItemHandler to manage our two stacks as it's convenient
+     */
+    private final ItemStackHandler frameContents = new ItemStackHandler(2) {
+        @Override
+        public boolean isItemValid(final int slotIndex, final ItemStack itemStack) {
+            if (plankSlot == slotIndex) {
+                return itemStack.is(FirmacivTags.Items.PLANKS);
+            }
+
+            if (boltSlot == slotIndex) {
+                return itemStack.is(FirmacivItems.COPPER_BOLT.get());
+            }
+
+            return super.isItemValid(slotIndex, itemStack);
+        }
+    };
 
     public WatercraftFrameBlockEntity(final BlockPos blockPos, final BlockState blockState) {
         super(FirmacivBlockEntities.WATERCRAFT_FRAME_BLOCK_ENTITY.get(), blockPos, blockState);
     }
 
-    public void loadAdditional(final CompoundTag compoundTag) {
-        planks = ItemStack.of(compoundTag.getCompound("Planks"));
-        bolts = ItemStack.of(compoundTag.getCompound("Bolts"));
-    }
-
     @Override
     public void saveAdditional(final CompoundTag compoundTag) {
-        compoundTag.put("Planks", planks.serializeNBT());
-        compoundTag.put("Bolts", bolts.serializeNBT());
+        compoundTag.put("Contents", frameContents.serializeNBT());
         super.saveAdditional(compoundTag);
     }
 
     @Override
     public final void load(final CompoundTag compoundTag) {
-        this.loadAdditional(compoundTag);
+        frameContents.deserializeNBT(compoundTag.getCompound("Contents"));
         super.load(compoundTag);
     }
 
@@ -38,43 +53,43 @@ public class WatercraftFrameBlockEntity extends BlockEntity {
      */
     public void ejectContents() {
         assert this.level != null : "Level should not be null";
-        Helpers.spawnItem(this.level, this.worldPosition, planks);
-        Helpers.spawnItem(this.level, this.worldPosition, bolts);
+        for (int slotIndex = 0; slotIndex < frameContents.getSlots(); slotIndex++) {
+            Helpers.spawnItem(this.level, this.worldPosition, frameContents.getStackInSlot(slotIndex));
+        }
     }
 
     /**
      * Adds the passed in stack to our plank total
      *
      * @param itemStack Stack of planks to add
+     * @return Remainder of the inserted stack
      */
-    public void insertPlanks(final ItemStack itemStack) {
-        if (planks.isEmpty()) {
-            planks = itemStack.copy();
-        } else planks.grow(itemStack.getCount());
+    public ItemStack insertPlanks(final ItemStack itemStack) {
+        return frameContents.insertItem(plankSlot, itemStack, false);
     }
 
     /**
      * Adds the passed in stack to our bolts total.
      *
      * @param itemStack Stack of bolts to add
+     * @return Remainder of the inserted stack
      */
-    public void insertBolts(final ItemStack itemStack) {
-        if (bolts.isEmpty()) {
-            bolts = itemStack.copy();
-        } else bolts.grow(itemStack.getCount());
+    @SuppressWarnings("UnusedReturnValue")
+    public ItemStack insertBolts(final ItemStack itemStack) {
+        return frameContents.insertItem(boltSlot, itemStack, false);
     }
 
     /**
      * Extracts planks from the amount we have
      */
     public ItemStack extractPlanks(final int amount) {
-        return planks.split(amount);
+        return frameContents.extractItem(plankSlot, amount, false);
     }
 
     /**
      * Extracts bolts from the amount we have
      */
     public ItemStack extractBolts(final int amount) {
-        return bolts.split(amount);
+        return frameContents.extractItem(boltSlot, amount, false);
     }
 }
