32add02f582ec4922d7be5214c864d29f7c59625
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-11-30 01:56:06 -0800

    Add final modifier to most variables and parameters. Renamed many parameters and some variables

--- src/main/java/com/hyperdash/firmaciv/common/block/SquaredAngleBlock.java
+++ src/main/java/com/hyperdash/firmaciv/common/block/SquaredAngleBlock.java
@@ -6,7 +6,6 @@ import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.*;
@@ -18,6 +17,7 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public abstract class SquaredAngleBlock extends Block implements SimpleWaterloggedBlock {
@@ -41,61 +41,62 @@ public abstract class SquaredAngleBlock extends Block implements SimpleWaterlogg
             OCTET_PPP);
     private static final int[] SHAPE_BY_STATE = new int[]{12, 5, 3, 10, 14, 13, 7, 11, 13, 7, 11, 14, 8, 4, 1, 2, 4, 1, 2, 8};
     // Forge Start
-    protected final java.util.function.Supplier<BlockState> stateSupplier;
+    protected final Supplier<BlockState> stateSupplier;
 
-    public SquaredAngleBlock(java.util.function.Supplier<BlockState> state, BlockBehaviour.Properties properties) {
+    public SquaredAngleBlock(final Supplier<BlockState> stateSupplier, final Properties properties) {
         super(properties);
         this.registerDefaultState(
                 this.stateDefinition.any().setValue(FACING, Direction.NORTH).setValue(HALF, Half.BOTTOM)
-                        .setValue(SHAPE, StairsShape.STRAIGHT).setValue(WATERLOGGED, Boolean.valueOf(false)));
-        this.stateSupplier = state;
+                        .setValue(SHAPE, StairsShape.STRAIGHT).setValue(WATERLOGGED, false));
+        this.stateSupplier = stateSupplier;
     }
 
-    private static VoxelShape[] makeShapes(VoxelShape pSlabShape, VoxelShape pNwCorner, VoxelShape pNeCorner,
-            VoxelShape pSwCorner, VoxelShape pSeCorner) {
-        return IntStream.range(0, 16).mapToObj((p_56945_) -> {
-            return makeStairShape(p_56945_, pSlabShape, pNwCorner, pNeCorner, pSwCorner, pSeCorner);
-        }).toArray((p_56949_) -> {
-            return new VoxelShape[p_56949_];
-        });
+    private static VoxelShape[] makeShapes(final VoxelShape slabShape, final VoxelShape northWestCorner,
+            final VoxelShape northEastCorner, final VoxelShape southWestCorner, final VoxelShape southEastCorner) {
+        return IntStream.range(0, 16).mapToObj(
+                (bitfield) -> makeStairShape(bitfield, slabShape, northWestCorner, northEastCorner, southWestCorner,
+                        southEastCorner)).toArray(VoxelShape[]::new);
     }
 
     /**
      * Combines the shapes according to the mode set in the bitfield
      */
-    private static VoxelShape makeStairShape(int pBitfield, VoxelShape pSlabShape, VoxelShape pNwCorner,
-            VoxelShape pNeCorner, VoxelShape pSwCorner, VoxelShape pSeCorner) {
+    private static VoxelShape makeStairShape(final int bitfield, final VoxelShape slabShape,
+            final VoxelShape northWestCorner, final VoxelShape northEastCorner, final VoxelShape southWestCorner,
+            final VoxelShape southEastCorner) {
 
-        double move = -0.5D;
-        if (pSlabShape == TOP_AABB) {
-            move = 0.5D;
+        final VoxelShape northWestCornerOpposite;
+        final VoxelShape northEastCornerOpposite;
+        final VoxelShape southWestCornerOpposite;
+        final VoxelShape southEastCornerOpposite;
+        {
+            final double move = slabShape == TOP_AABB ? 0.5 : -0.5;
+
+            northWestCornerOpposite = northWestCorner.move(0, move, 0);
+            northEastCornerOpposite = northEastCorner.move(0, move, 0);
+            southWestCornerOpposite = southWestCorner.move(0, move, 0);
+            southEastCornerOpposite = southEastCorner.move(0, move, 0);
         }
 
-        VoxelShape NwCornerOpposite = pNwCorner.move(0, move, 0);
-        VoxelShape NeCornerOpposite = pNeCorner.move(0, move, 0);
-        VoxelShape SwCornerOpposite = pSwCorner.move(0, move, 0);
-        VoxelShape SeCornerOpposite = pSeCorner.move(0, move, 0);
-
-
-        VoxelShape voxelshape = pSlabShape;
-        if ((pBitfield & 1) != 0) {
-            voxelshape = Shapes.or(pSlabShape, pNwCorner);
-            voxelshape = Shapes.join(voxelshape, NwCornerOpposite, BooleanOp.ONLY_FIRST);
+        VoxelShape voxelshape = slabShape;
+        if ((bitfield & 1) != 0) {
+            voxelshape = Shapes.or(slabShape, northWestCorner);
+            voxelshape = Shapes.join(voxelshape, northWestCornerOpposite, BooleanOp.ONLY_FIRST);
         }
 
-        if ((pBitfield & 2) != 0) {
-            voxelshape = Shapes.or(voxelshape, pNeCorner);
-            voxelshape = Shapes.join(voxelshape, NeCornerOpposite, BooleanOp.ONLY_FIRST);
+        if ((bitfield & 2) != 0) {
+            voxelshape = Shapes.or(voxelshape, northEastCorner);
+            voxelshape = Shapes.join(voxelshape, northEastCornerOpposite, BooleanOp.ONLY_FIRST);
         }
 
-        if ((pBitfield & 4) != 0) {
-            voxelshape = Shapes.or(voxelshape, pSwCorner);
-            voxelshape = Shapes.join(voxelshape, SwCornerOpposite, BooleanOp.ONLY_FIRST);
+        if ((bitfield & 4) != 0) {
+            voxelshape = Shapes.or(voxelshape, southWestCorner);
+            voxelshape = Shapes.join(voxelshape, southWestCornerOpposite, BooleanOp.ONLY_FIRST);
         }
 
-        if ((pBitfield & 8) != 0) {
-            voxelshape = Shapes.or(voxelshape, pSeCorner);
-            voxelshape = Shapes.join(voxelshape, SeCornerOpposite, BooleanOp.ONLY_FIRST);
+        if ((bitfield & 8) != 0) {
+            voxelshape = Shapes.or(voxelshape, southEastCorner);
+            voxelshape = Shapes.join(voxelshape, southEastCornerOpposite, BooleanOp.ONLY_FIRST);
         }
 
         return voxelshape;
@@ -104,13 +105,14 @@ public abstract class SquaredAngleBlock extends Block implements SimpleWaterlogg
     /**
      * Returns a stair shape property based on the surrounding stairs from the given blockstate and position
      */
-    private static StairsShape getStairsShape(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
-        Direction direction = pState.getValue(FACING);
-        BlockState blockstate = pLevel.getBlockState(pPos.relative(direction));
-        if (isRoof(blockstate) && pState.getValue(HALF) == blockstate.getValue(HALF)) {
-            Direction direction1 = blockstate.getValue(FACING);
-            if (direction1.getAxis() != pState.getValue(FACING).getAxis() && canTakeShape(pState, pLevel, pPos,
-                    direction1.getOpposite())) {
+    private static StairsShape getStairsShape(final BlockState blockState, final BlockGetter blockGetter,
+            final BlockPos blockPos) {
+        final Direction direction = blockState.getValue(FACING);
+        final BlockState blockstate = blockGetter.getBlockState(blockPos.relative(direction));
+        if (isRoof(blockstate) && blockState.getValue(HALF) == blockstate.getValue(HALF)) {
+            final Direction direction1 = blockstate.getValue(FACING);
+            if (direction1.getAxis() != blockState.getValue(FACING).getAxis() && canTakeShape(blockState, blockGetter,
+                    blockPos, direction1.getOpposite())) {
                 if (direction1 == direction.getCounterClockWise()) {
                     return StairsShape.OUTER_LEFT;
                 }
@@ -119,11 +121,11 @@ public abstract class SquaredAngleBlock extends Block implements SimpleWaterlogg
             }
         }
 
-        BlockState blockstate1 = pLevel.getBlockState(pPos.relative(direction.getOpposite()));
-        if (isRoof(blockstate1) && pState.getValue(HALF) == blockstate1.getValue(HALF)) {
-            Direction direction2 = blockstate1.getValue(FACING);
-            if (direction2.getAxis() != pState.getValue(FACING).getAxis() && canTakeShape(pState, pLevel, pPos,
-                    direction2)) {
+        final BlockState blockstate1 = blockGetter.getBlockState(blockPos.relative(direction.getOpposite()));
+        if (isRoof(blockstate1) && blockState.getValue(HALF) == blockstate1.getValue(HALF)) {
+            final Direction direction2 = blockstate1.getValue(FACING);
+            if (direction2.getAxis() != blockState.getValue(FACING).getAxis() && canTakeShape(blockState, blockGetter,
+                    blockPos, direction2)) {
                 if (direction2 == direction.getCounterClockWise()) {
                     return StairsShape.INNER_LEFT;
                 }
@@ -135,118 +137,123 @@ public abstract class SquaredAngleBlock extends Block implements SimpleWaterlogg
         return StairsShape.STRAIGHT;
     }
 
-    private static boolean canTakeShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, Direction pFace) {
-        BlockState blockstate = pLevel.getBlockState(pPos.relative(pFace));
-        return !isRoof(blockstate) || blockstate.getValue(FACING) != pState.getValue(FACING) || blockstate.getValue(
-                HALF) != pState.getValue(HALF);
+    private static boolean canTakeShape(final BlockState blockState, final BlockGetter blockGetter,
+            final BlockPos blockPos, final Direction direction) {
+        final BlockState blockstate = blockGetter.getBlockState(blockPos.relative(direction));
+        return !isRoof(blockstate) || blockstate.getValue(FACING) != blockState.getValue(FACING) || blockstate.getValue(
+                HALF) != blockState.getValue(HALF);
     }
 
-    public static boolean isRoof(BlockState pState) {
-        return pState.getBlock() instanceof SquaredAngleBlock;
+    public static boolean isRoof(final BlockState blockState) {
+        return blockState.getBlock() instanceof SquaredAngleBlock;
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public boolean useShapeForLightOcclusion(BlockState pState) {
+    public boolean useShapeForLightOcclusion(final BlockState blockState) {
         return true;
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
-        return (pState.getValue(HALF) == Half.TOP ? TOP_SHAPES : BOTTOM_SHAPES)[SHAPE_BY_STATE[this.getShapeIndex(
-                pState)]];
+    public VoxelShape getShape(final BlockState blockState, final BlockGetter blockGetter, final BlockPos blockPos,
+            final CollisionContext context) {
+        return (blockState.getValue(HALF) == Half.TOP ? TOP_SHAPES : BOTTOM_SHAPES)[SHAPE_BY_STATE[this.getShapeIndex(
+                blockState)]];
     }
 
-    private int getShapeIndex(BlockState pState) {
-        return pState.getValue(SHAPE).ordinal() * 4 + pState.getValue(FACING).get2DDataValue();
+    private int getShapeIndex(final BlockState blockState) {
+        return blockState.getValue(SHAPE).ordinal() * 4 + blockState.getValue(FACING).get2DDataValue();
     }
 
     @Override
-    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        Direction direction = pContext.getClickedFace();
-        BlockPos blockpos = pContext.getClickedPos();
-        FluidState fluidstate = pContext.getLevel().getFluidState(blockpos);
-        BlockState blockstate = this.defaultBlockState().setValue(FACING, pContext.getHorizontalDirection())
+    public BlockState getStateForPlacement(final BlockPlaceContext placeContext) {
+        final Direction direction = placeContext.getClickedFace();
+        final BlockPos blockpos = placeContext.getClickedPos();
+        final FluidState fluidstate = placeContext.getLevel().getFluidState(blockpos);
+        final BlockState blockstate = this.defaultBlockState().setValue(FACING, placeContext.getHorizontalDirection())
                 .setValue(HALF, Half.BOTTOM).setValue(WATERLOGGED, fluidstate.getType() == Fluids.WATER);
-        return blockstate.setValue(SHAPE, getStairsShape(blockstate, pContext.getLevel(), blockpos));
+
+        return blockstate.setValue(SHAPE, getStairsShape(blockstate, placeContext.getLevel(), blockpos));
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public BlockState updateShape(BlockState pState, Direction pFacing, BlockState pFacingState, LevelAccessor pLevel,
-            BlockPos pCurrentPos, BlockPos pFacingPos) {
-        if (pState.getValue(WATERLOGGED)) {
-            pLevel.scheduleTick(pCurrentPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+    public BlockState updateShape(final BlockState blockState, final Direction direction,
+            final BlockState neighborState, final LevelAccessor levelAccessor, final BlockPos blockPos,
+            final BlockPos neighborPos) {
+
+        if (blockState.getValue(WATERLOGGED)) {
+            levelAccessor.scheduleTick(blockPos, Fluids.WATER, Fluids.WATER.getTickDelay(levelAccessor));
         }
 
-        return pFacing.getAxis().isHorizontal() ? pState.setValue(SHAPE,
-                getStairsShape(pState, pLevel, pCurrentPos)) : super.updateShape(pState, pFacing, pFacingState, pLevel,
-                pCurrentPos, pFacingPos);
+        if (direction.getAxis().isHorizontal())
+            return blockState.setValue(SHAPE, getStairsShape(blockState, levelAccessor, blockPos));
+
+        return super.updateShape(blockState, direction, neighborState, levelAccessor, blockPos, neighborPos);
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public BlockState rotate(BlockState pState, Rotation pRot) {
-        return pState.setValue(FACING, pRot.rotate(pState.getValue(FACING)));
+    public BlockState rotate(final BlockState blockState, final Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public BlockState mirror(BlockState pState, Mirror pMirror) {
-        Direction direction = pState.getValue(FACING);
-        StairsShape stairsshape = pState.getValue(SHAPE);
-        switch (pMirror) {
+    public BlockState mirror(final BlockState blockState, final Mirror mirror) {
+        final Direction direction = blockState.getValue(FACING);
+        final StairsShape stairsshape = blockState.getValue(SHAPE);
+        switch (mirror) {
             case LEFT_RIGHT:
                 if (direction.getAxis() == Direction.Axis.Z) {
-                    switch (stairsshape) {
-                        case INNER_LEFT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_RIGHT);
-                        case INNER_RIGHT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_LEFT);
-                        case OUTER_LEFT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_RIGHT);
-                        case OUTER_RIGHT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_LEFT);
-                        default:
-                            return pState.rotate(Rotation.CLOCKWISE_180);
-                    }
+                    return switch (stairsshape) {
+                        case INNER_LEFT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_RIGHT);
+                        case INNER_RIGHT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_LEFT);
+                        case OUTER_LEFT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_RIGHT);
+                        case OUTER_RIGHT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_LEFT);
+                        default -> blockState.rotate(Rotation.CLOCKWISE_180);
+                    };
                 }
                 break;
             case FRONT_BACK:
                 if (direction.getAxis() == Direction.Axis.X) {
-                    switch (stairsshape) {
-                        case INNER_LEFT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_LEFT);
-                        case INNER_RIGHT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_RIGHT);
-                        case OUTER_LEFT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_RIGHT);
-                        case OUTER_RIGHT:
-                            return pState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_LEFT);
-                        case STRAIGHT:
-                            return pState.rotate(Rotation.CLOCKWISE_180);
-                    }
+                    return switch (stairsshape) {
+                        case INNER_LEFT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_LEFT);
+                        case INNER_RIGHT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.INNER_RIGHT);
+                        case OUTER_LEFT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_RIGHT);
+                        case OUTER_RIGHT ->
+                                blockState.rotate(Rotation.CLOCKWISE_180).setValue(SHAPE, StairsShape.OUTER_LEFT);
+                        case STRAIGHT -> blockState.rotate(Rotation.CLOCKWISE_180);
+                    };
                 }
         }
 
-        return super.mirror(pState, pMirror);
+        return super.mirror(blockState, mirror);
     }
 
     @Override
-    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(FACING, HALF, SHAPE, WATERLOGGED);
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public FluidState getFluidState(BlockState pState) {
-        return pState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(pState);
+    protected void createBlockStateDefinition(final StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING, HALF, SHAPE, WATERLOGGED);
     }
 
     @Override
     @SuppressWarnings("deprecation")
-    public boolean isPathfindable(BlockState pState, BlockGetter pLevel, BlockPos pPos, PathComputationType pType) {
+    public FluidState getFluidState(final BlockState blockState) {
+        return blockState.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(blockState);
+    }
+
+    @Override
+    @SuppressWarnings("deprecation")
+    public boolean isPathfindable(final BlockState blockState, final BlockGetter blockGetter, final BlockPos blockPos,
+            final PathComputationType computationType) {
         return false;
     }
 
