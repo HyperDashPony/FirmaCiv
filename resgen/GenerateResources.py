3533a7c0a149d32f6cf88dfd7806a3f4c668a7ba
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-11-30 20:29:01 -0800

    Add Constants.py to store our default non generated lang in

--- resgen/GenerateResources.py
+++ resgen/GenerateResources.py
@@ -2,6 +2,7 @@ import mcresources
 from mcresources import ResourceManager
 
 import BlockStates
+import Constants
 
 WOODS = {"acacia": "Acacia",
          "ash": "Ash",
@@ -46,6 +47,7 @@ def generateBlockModels(manager: ResourceManager):
 def main():
     resourceManager = mcresources.ResourceManager("firmaciv", "../src/main/resources",
                                                   on_error=lambda file, e: print(f"Error writing {file}\n{e}"))
+    resourceManager.lang(Constants.DEFAULT_LANG)
     generateBlockModels(resourceManager)
     resourceManager.flush()
     print("Generated stuff!")
