8477c9966c360572c80e664841bb5e1bc27a394e
alekiponi <hyperdashpony@gmail.com>
2023-12-02 10:52:13 -0800

    Add pants to player pose mixin
    closes #18

--- src/main/java/com/alekiponi/firmaciv/mixins/minecraft/PlayerModelMixin.java
+++ src/main/java/com/alekiponi/firmaciv/mixins/minecraft/PlayerModelMixin.java
@@ -8,13 +8,19 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerModel.class)
 public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
+
+    @Shadow @Final public ModelPart rightPants;
+
+    @Shadow @Final public ModelPart leftPants;
 
     public PlayerModelMixin(ModelPart pRoot) {
         super(pRoot);
@@ -35,8 +41,19 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
                     this.leftLeg.yRot = -0.002F;
                     this.leftLeg.zRot = -0.002F;
 
+                    this.rightPants.xRot = -1.570796F;
+                    this.rightPants.yRot = 0F;
+                    this.rightPants.zRot = 0F;
+
+                    this.leftPants.xRot = -1.570796F;
+                    this.leftPants.yRot = -0.002F;
+                    this.leftPants.zRot = -0.002F;
+
                     this.rightLeg.setPos(-1.9F, 11.5F, 0.0F);
                     this.leftLeg.setPos(01.9F, 11.5F, 0.0F);
+
+                    this.rightPants.setPos(-1.9F, 11.6F, 0F);
+                    this.leftPants.setPos(01.9F, 11.6F, 0F);
                 }
 
             } else if (emptyCompartmentEntity.getTrueVehicle() instanceof KayakEntity) {
@@ -49,19 +66,37 @@ public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
                     this.leftLeg.yRot = 0.1570796F;
                     this.leftLeg.zRot = -0F;
 
+                    this.rightPants.xRot = -1.570796F;
+                    this.rightPants.yRot = -0.1570796F;
+                    this.rightPants.zRot = 0F;
+
+                    this.leftPants.xRot = -1.570796F;
+                    this.leftPants.yRot = 0.1570796F;
+                    this.leftPants.zRot = -0F;
+
                     this.rightLeg.setPos(-1.9F, 13.6F, 1F);
                     this.leftLeg.setPos(01.9F, 13.6F, 1F);
+
+                    this.rightPants.setPos(-1.9F, 13.6F, 1F);
+                    this.leftPants.setPos(01.9F, 13.6F, 1F);
                 }
             }
 
         } else {
             this.rightLeg.setPos(-1.9F, 12.0F, 0.0F);
             this.leftLeg.setPos(01.9F, 12.0F, 0.0F);
+            this.rightPants.setPos(-1.9F, 12.0F, 0.0F);
+            this.leftPants.setPos(01.9F, 12.0F, 0.0F);
             if (this.crouching) {
                 this.rightLeg.z = 4.0F;
                 this.leftLeg.z = 4.0F;
                 this.rightLeg.y = 12.2F;
                 this.leftLeg.y = 12.2F;
+
+                this.rightPants.z = 4.0F;
+                this.leftPants.z = 4.0F;
+                this.rightPants.y = 12.2F;
+                this.leftPants.y = 12.2F;
             }
         }
 
