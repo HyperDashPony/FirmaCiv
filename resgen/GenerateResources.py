3a0f5afeb6e9e34f89cf8301e43bdf2818ce3261
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 05:54:17 -0800

    Better stats reporting for resource generation

--- resgen/GenerateResources.py
+++ resgen/GenerateResources.py
@@ -7,10 +7,14 @@ import constants
 def main():
     resourceManager = mcresources.ResourceManager("firmaciv", "../src/main/resources",
                                                   on_error=lambda file, e: print(f"Error writing {file}\n{e}"))
+    print("Starting resource generation.")
     resourceManager.lang(constants.DEFAULT_LANG)
     assets.generate(resourceManager)
     resourceManager.flush()
-    print("Generated stuff!")
+
+    print(f"Finished generating files!")
+    print(f"New: {resourceManager.new_files}, Modified: {resourceManager.modified_files},"
+          f" Unchanged: {resourceManager.unchanged_files}, Errors: {resourceManager.error_files}")
 
 
 if __name__ == '__main__':
