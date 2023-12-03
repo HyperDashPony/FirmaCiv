200a3e558c2bb086a4fa06ec245ecc05a554dec5
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 05:50:24 -0800

    Generate canoe component lang as well as block loot

--- resgen/assets.py
+++ resgen/assets.py
@@ -31,7 +31,8 @@ def generate(manager: ResourceManager):
                                 f"firmaciv:block/canoe_component_block/template/end/{n}")
             manager.block_model(f"wood/canoe_component_block/{wood}/middle/{n}", canoe_component_textures,
                                 f"firmaciv:block/canoe_component_block/template/middle/{n}")
-            manager.blockstate(f"canoe_component_block/{wood}", variants=blockStates.canoe_component(wood))
+            manager.blockstate(f"canoe_component_block/{wood}", variants=blockStates.canoe_component(wood)).with_lang(
+                f"{name} Canoe Component").with_block_loot(f"tfc:wood/lumber/{wood}")
 
         # Basic frame
         manager.blockstate("watercraft_frame_angled", variants=blockStates.angledWaterCraftFrame).with_lang(
