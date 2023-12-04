21a09729ebfcd296fc8d482265f34e466b424edc
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 16:21:01 -0800

    Generate tags more smartly

--- resgen/data.py
+++ resgen/data.py
@@ -6,7 +6,11 @@ import constants
 def generate(manager: ResourceManager):
     # Tags with all wood types
     for wood in constants.TFC_WOODS.keys():
-        manager.tag("canoe_component_blocks", "blocks", f"canoe_component_block/{wood}")
-        manager.tag("can_make_canoe_unrestricted", "blocks", f"tfc:wood/stripped_log/{wood}")
-        manager.tag("wooden_watercraft_frames", "blocks", f"wood/{wood}/watercraft_frame_angled")
-        manager.tag("tfc:mineable_with_blunt_tool", "blocks", f"canoe_component_block/{wood}")
+        manager.block_tag("canoe_component_blocks", f"canoe_component_block/{wood}")
+        manager.block_tag("can_make_canoe_unrestricted", f"tfc:wood/stripped_log/{wood}")
+        manager.block_tag("wooden_watercraft_frames", f"wood/{wood}/watercraft_frame_angled")
+
+    manager.block_tag("tfc:mineable_with_blunt_tool", "#firmaciv:canoe_component_blocks")
+    manager.block_tag("minecraft:mineable/axe", "watercraft_frame_angled", "#firmaciv:canoe_component_blocks",
+                      "#firmaciv:wooden_watercraft_frames")
+    manager.block_tag("minecraft:mineable/pickaxe", "oarlock")
