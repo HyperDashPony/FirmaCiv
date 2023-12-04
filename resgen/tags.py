7559ee0ce387df505701727811379909b6d38e4f
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 22:04:04 -0800

    Make tags use the new registry names as well

--- resgen/tags.py
+++ resgen/tags.py
@@ -6,9 +6,9 @@ import constants
 def generate(manager: ResourceManager):
     # Tags with all wood types
     for wood in constants.TFC_WOODS.keys():
-        manager.block_tag("canoe_component_blocks", f"canoe_component_block/{wood}")
+        manager.block_tag("canoe_component_blocks", f"wood/canoe_component_block/{wood}")
         manager.block_tag("can_make_canoe_unrestricted", f"tfc:wood/stripped_log/{wood}")
-        manager.block_tag("wooden_watercraft_frames", f"wood/{wood}/watercraft_frame_angled")
+        manager.block_tag("wooden_watercraft_frames", f"wood/watercraft_frame_angled/{wood}")
 
     # Vanilla mining tags
     manager.block_tag("minecraft:mineable/axe", "watercraft_frame_angled", "#firmaciv:canoe_component_blocks",
