26bcc0a59e851917aef07c419b0ad1ac69dc41cb
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 19:46:37 -0800

    Remove some old generation files

--- resgen/lootTables.py
+++
@@ -1,9 +0,0 @@
-from mcresources import ResourceManager
-from mcresources import loot_tables
-
-import blockStates
-import constants
-
-def generate(manager: ResourceManager):
-    for wood, name in constants.TFC_WOODS.items():
-        manager.block_loot(f"wood/{wood}/watercraft_frame_angled")
