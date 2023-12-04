31189eb25463604495450df59c794f2e7ef51120
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-03 16:31:57 -0800

    Add stuff to the tfc usable_on_tool_rack tag

--- resgen/data.py
+++ resgen/data.py
@@ -10,7 +10,12 @@ def generate(manager: ResourceManager):
         manager.block_tag("can_make_canoe_unrestricted", f"tfc:wood/stripped_log/{wood}")
         manager.block_tag("wooden_watercraft_frames", f"wood/{wood}/watercraft_frame_angled")
 
-    manager.block_tag("tfc:mineable_with_blunt_tool", "#firmaciv:canoe_component_blocks")
+    # Vanilla mining tags
     manager.block_tag("minecraft:mineable/axe", "watercraft_frame_angled", "#firmaciv:canoe_component_blocks",
                       "#firmaciv:wooden_watercraft_frames")
     manager.block_tag("minecraft:mineable/pickaxe", "oarlock")
+
+    # TFC tags
+    manager.block_tag("tfc:mineable_with_blunt_tool", "#firmaciv:canoe_component_blocks")
+    manager.item_tag("tfc:usable_on_tool_rack", "canoe_paddle", "kayak_paddle", "oar", "kayak", "nav_clock", "sextant",
+                     "barometer")
