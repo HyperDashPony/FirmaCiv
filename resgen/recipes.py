6c79a6dfecef4a64459d16decdcfe5964b2e4fd5
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-05 02:55:34 -0800

    Rename variables

--- resgen/recipes.py
+++ resgen/recipes.py
@@ -4,22 +4,22 @@ from mcresources.type_definitions import ResourceIdentifier
 import constants
 
 
-def generate(manager: ResourceManager):
+def generate(rm: ResourceManager):
     def disableRecipe(name_parts: ResourceIdentifier):
-        manager.recipe(name_parts, None, {}, conditions="forge:false")
+        rm.recipe(name_parts, None, {}, conditions="forge:false")
 
     # Disable TFC boat recipes
-    for wood in constants.TFC_WOODS.keys():
-        disableRecipe(f"tfc:crafting/wood/{wood}_boat")
+    for woodType in constants.TFC_WOODS.keys():
+        disableRecipe(f"tfc:crafting/wood/{woodType}_boat")
 
     # Disable vanilla boat recipes
-    for wood in ["acacia", "birch", "cherry", "dark_oak", "jungle", "mangrove", "oak", "spruce"]:
-        disableRecipe(f"minecraft:{wood}_boat")
-        disableRecipe(f"minecraft:{wood}_chest_boat")
+    for woodType in ["acacia", "birch", "cherry", "dark_oak", "jungle", "mangrove", "oak", "spruce"]:
+        disableRecipe(f"minecraft:{woodType}_boat")
+        disableRecipe(f"minecraft:{woodType}_chest_boat")
     # Bamboo raft as well
     disableRecipe("minecraft:bamboo_raft")
 
-    manager.crafting_shaped("minecraft:compass", ["X", "Y", "Z"], {
+    rm.crafting_shaped("minecraft:compass", ["X", "Y", "Z"], {
         "X": {
             "item": "tfc:lens"
         },
