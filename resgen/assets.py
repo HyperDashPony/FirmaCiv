fdc118ad70b141419175d29f7da880fcb0e5f7e8
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-05 04:09:05 -0800

    Rename variables

--- resgen/assets.py
+++ resgen/assets.py
@@ -5,45 +5,45 @@ import constants
 import lootTables
 
 
-def generate(manager: ResourceManager):
-    for wood, name in constants.TFC_WOODS.items():
+def generate(rm: ResourceManager):
+    for woodType, name in constants.TFC_WOODS.items():
         # Generate models from templates
         for shape in ["straight", "inner", "outer"]:
             for progress in ["first", "second", "third", "fourth"]:
-                manager.block_model(f"wood/watercraft_frame_angled/{wood}/{shape}/{progress}",
-                                    {"plank": f"tfc:block/wood/planks/{wood}"},
-                                    f"firmaciv:block/watercraft_frame_angled/template/{shape}/{progress}")
+                rm.block_model(f"wood/watercraft_frame_angled/{woodType}/{shape}/{progress}",
+                               {"plank": f"tfc:block/wood/planks/{woodType}"},
+                               f"firmaciv:block/watercraft_frame_angled/template/{shape}/{progress}")
 
-        manager.blockstate_multipart(f"wood/watercraft_frame_angled/{wood}",
-                                     *blockStates.getWoodFrameMultipart(wood)).with_lang(
-            f"{name} Shipwright's Scaffolding").with_block_loot(*lootTables.boat_frame(wood))
+        rm.blockstate_multipart(f"wood/watercraft_frame_angled/{woodType}",
+                                *blockStates.getWoodFrameMultipart(woodType)).with_lang(
+            f"{name} Shipwright's Scaffolding").with_block_loot(*lootTables.boat_frame(woodType))
 
         # Canoe components now
-        canoe_component_textures = {"0": f"tfc:block/wood/stripped_log/{wood}",
-                                    "1": "tfc:block/wood/stripped_log_top/acacia",
-                                    "particle": "tfc:block/wood/stripped_log/acacia"}
+        canoe_component_textures = {"0": f"tfc:block/wood/stripped_log/{woodType}",
+                                    "1": f"tfc:block/wood/stripped_log_top/{woodType}",
+                                    "particle": f"tfc:block/wood/stripped_log/{woodType}"}
         # Models that are shared by the end and middle states
         for n in range(8):
-            manager.block_model(f"wood/canoe_component_block/{wood}/all/{n}", canoe_component_textures,
-                                f"firmaciv:block/canoe_component_block/template/all/{n}")
+            rm.block_model(f"wood/canoe_component_block/{woodType}/all/{n}", canoe_component_textures,
+                           f"firmaciv:block/canoe_component_block/template/all/{n}")
         # End and Middle only models
         for n in range(8, 13):
-            manager.block_model(f"wood/canoe_component_block/{wood}/end/{n}", canoe_component_textures,
-                                f"firmaciv:block/canoe_component_block/template/end/{n}")
-            manager.block_model(f"wood/canoe_component_block/{wood}/middle/{n}", canoe_component_textures,
-                                f"firmaciv:block/canoe_component_block/template/middle/{n}")
-            manager.blockstate(f"wood/canoe_component_block/{wood}",
-                               variants=blockStates.canoe_component(wood)).with_lang(
-                f"{name} Canoe Component").with_block_loot(f"tfc:wood/lumber/{wood}")
+            rm.block_model(f"wood/canoe_component_block/{woodType}/end/{n}", canoe_component_textures,
+                           f"firmaciv:block/canoe_component_block/template/end/{n}")
+            rm.block_model(f"wood/canoe_component_block/{woodType}/middle/{n}", canoe_component_textures,
+                           f"firmaciv:block/canoe_component_block/template/middle/{n}")
+            rm.blockstate(f"wood/canoe_component_block/{woodType}",
+                          variants=blockStates.canoe_component(woodType)).with_lang(
+                f"{name} Canoe Component").with_block_loot(f"tfc:wood/lumber/{woodType}")
 
     # Basic frame
-    manager.blockstate("watercraft_frame_angled", variants=blockStates.angledWaterCraftFrame).with_lang(
+    rm.blockstate("watercraft_frame_angled", variants=blockStates.angledWaterCraftFrame).with_lang(
         "Shipwright's Scaffolding").with_block_loot("firmaciv:watercraft_frame_angled")
     # Need to manually make the model
-    manager.item_model("watercraft_frame_angled", parent="firmaciv:block/watercraft_frame_angled/straight",
-                       no_textures=True)
+    rm.item_model("watercraft_frame_angled", parent="firmaciv:block/watercraft_frame_angled/straight",
+                  no_textures=True)
 
-    manager.blockstate("oarlock", variants={
+    rm.blockstate("oarlock", variants={
         "facing=east": {
             "model": "firmaciv:block/oarlock",
             "y": 90
@@ -60,23 +60,23 @@ def generate(manager: ResourceManager):
             "y": 270
         }
     }).with_lang("Oarlock").with_block_loot("firmaciv:oarlock")
-    manager.item_model("oarlock")
+    rm.item_model("oarlock")
 
     # Items with generated models
-    manager.item("unfinished_barometer").with_item_model().with_lang("Unfinished Barometer")
-    manager.item("unfinished_nav_clock").with_item_model().with_lang("Unfinished Navigator's Timepiece")
-    manager.item("unfinished_sextant").with_item_model().with_lang("Unfinished Sextant")
-    manager.item("sextant").with_item_model().with_lang("Sextant")
-    manager.item("copper_bolt").with_item_model().with_lang("Copper Bolt")
-    manager.item("kayak").with_item_model().with_lang("Kayak")
-    manager.item("large_waterproof_hide").with_item_model().with_lang("Large Waterproof Hide")
-    manager.item("nav_toolkit").with_item_model().with_lang("Navigator's Toolkit")
+    rm.item("unfinished_barometer").with_item_model().with_lang("Unfinished Barometer")
+    rm.item("unfinished_nav_clock").with_item_model().with_lang("Unfinished Navigator's Timepiece")
+    rm.item("unfinished_sextant").with_item_model().with_lang("Unfinished Sextant")
+    rm.item("sextant").with_item_model().with_lang("Sextant")
+    rm.item("copper_bolt").with_item_model().with_lang("Copper Bolt")
+    rm.item("kayak").with_item_model().with_lang("Kayak")
+    rm.item("large_waterproof_hide").with_item_model().with_lang("Large Waterproof Hide")
+    rm.item("nav_toolkit").with_item_model().with_lang("Navigator's Toolkit")
 
     # Items with custom models
-    manager.item("barometer").with_lang("Barometer")
-    manager.item("nav_clock").with_lang("Navigator's Timepiece")
-    manager.item("firmaciv_compass").with_lang("Compass (Declination: True North)")
+    rm.item("barometer").with_lang("Barometer")
+    rm.item("nav_clock").with_lang("Navigator's Timepiece")
+    rm.item("firmaciv_compass").with_lang("Compass (Declination: True North)")
 
-    manager.item("kayak_paddle").with_lang("Kayak Paddle")
-    manager.item("canoe_paddle").with_lang("Canoe Paddle")
-    manager.item("oar").with_lang("Oar")
+    rm.item("kayak_paddle").with_lang("Kayak Paddle")
+    rm.item("canoe_paddle").with_lang("Canoe Paddle")
+    rm.item("oar").with_lang("Oar")
