fd35216c23897a93a19c474f3d01bee91db99f95
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-09 03:25:52 -0800

    TODO on weird blockstate property

--- src/main/java/com/alekiponi/firmaciv/common/block/FirmacivBlockStateProperties.java
+++ src/main/java/com/alekiponi/firmaciv/common/block/FirmacivBlockStateProperties.java
@@ -4,6 +4,7 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
 public class FirmacivBlockStateProperties {
+    // TODO state 13 not actually used? Also why are we starting from 1
     public static final IntegerProperty CANOE_CARVED = IntegerProperty.create("canoe_carved", 1, 13);
     public static final IntegerProperty FRAME_PROCESSED = IntegerProperty.create("frame_processed", 0, 7);
     public static final BooleanProperty END = BooleanProperty.create("end");
