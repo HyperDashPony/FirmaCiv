60520e9b2ba0aeeecf571c27f966cd7813b7e4af
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-21 23:04:32 -0800

    Touch up WorkbenchCompartmentEntity

--- src/main/java/com/alekiponi/firmaciv/common/entity/vehiclehelper/compartment/WorkbenchCompartmentEntity.java
+++ src/main/java/com/alekiponi/firmaciv/common/entity/vehiclehelper/compartment/WorkbenchCompartmentEntity.java
@@ -7,20 +7,24 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.HasCustomInventoryScreen;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 import java.util.function.BiFunction;
 
-public class WorkbenchCompartmentEntity extends AbstractCompartmentEntity implements HasCustomInventoryScreen {
+public class WorkbenchCompartmentEntity extends AbstractCompartmentEntity implements HasCustomInventoryScreen, MenuProvider {
     private static final Component CONTAINER_TITLE = Component.translatable("container.crafting");
 
-    public WorkbenchCompartmentEntity(final EntityType<?> entityType, final Level level) {
+    public WorkbenchCompartmentEntity(final EntityType<? extends WorkbenchCompartmentEntity> entityType,
+            final Level level) {
         super(entityType, level);
     }
 
@@ -34,30 +38,31 @@ public class WorkbenchCompartmentEntity extends AbstractCompartmentEntity implem
     }
 
     @Override
+    public Component getDisplayName() {
+        return CONTAINER_TITLE;
+    }
+
+    @Override
     public InteractionResult interact(final Player player, final InteractionHand hand) {
         this.openCustomInventoryScreen(player);
         return InteractionResult.SUCCESS;
     }
 
     @Override
-    public void openCustomInventoryScreen(final Player player) {
-        // Stupid, gross, dirty way of letting vanilla do most of the container work
-        class WorkbenchLevelAccess implements ContainerLevelAccess {
-            private final WorkbenchCompartmentEntity compartment;
-
-            public WorkbenchLevelAccess(final WorkbenchCompartmentEntity compartment) {
-                this.compartment = compartment;
-            }
-
+    public @Nullable AbstractContainerMenu createMenu(final int id, final Inventory playerInventory,
+            final Player player) {
+        return new CompartmentCraftingMenu(id, playerInventory, new ContainerLevelAccess() {
             @Override
             public <T> Optional<T> evaluate(final BiFunction<Level, BlockPos, T> function) {
-                return Optional.of(function.apply(compartment.level(), compartment.blockPosition()));
+                return Optional.of(function.apply(WorkbenchCompartmentEntity.this.level(),
+                        WorkbenchCompartmentEntity.this.blockPosition()));
             }
-        }
+        });
+    }
 
-        player.openMenu(new SimpleMenuProvider(
-                (containerID, playerInventory, unused) -> new CompartmentCraftingMenu(containerID, playerInventory,
-                        new WorkbenchLevelAccess(this)), CONTAINER_TITLE));
+    @Override
+    public void openCustomInventoryScreen(final Player player) {
+        player.openMenu(this);
         player.awardStat(Stats.INTERACT_WITH_CRAFTING_TABLE);
     }
 }
