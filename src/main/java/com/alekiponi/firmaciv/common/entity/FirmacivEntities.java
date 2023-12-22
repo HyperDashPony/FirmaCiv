c8c39c6a25ca6d4d57eac7b5706bdc57ecf3e78c
Traister101 <66151907+Traister101@users.noreply.github.com>
2023-12-21 23:56:35 -0800

    Add CompartmentType which allows us to dynamically create Compartments from an ItemStack

--- src/main/java/com/alekiponi/firmaciv/common/entity/FirmacivEntities.java
+++ src/main/java/com/alekiponi/firmaciv/common/entity/FirmacivEntities.java
@@ -13,29 +13,33 @@ import com.alekiponi.firmaciv.common.entity.vehiclehelper.compartment.WorkbenchC
 import com.alekiponi.firmaciv.util.BoatVariant;
 import net.dries007.tfc.util.Helpers;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.Locale;
 import java.util.Map;
+
+import static com.alekiponi.firmaciv.Firmaciv.MOD_ID;
 
 public final class FirmacivEntities {
 
     private static final int LARGE_VEHICLE_TRACKING = 20;
-    private static final int VEHICLE_HELPER_TRACKING = LARGE_VEHICLE_TRACKING+1;
-    public static DeferredRegister<EntityType<?>> ENTITY_TYPES
-            = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, Firmaciv.MOD_ID);
+    private static final int VEHICLE_HELPER_TRACKING = LARGE_VEHICLE_TRACKING + 1;
+    public static DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES,
+            Firmaciv.MOD_ID);
 
-    public static final Map<BoatVariant, RegistryObject<EntityType<CanoeEntity>>> CANOES =
-            Helpers.mapOfKeys(BoatVariant.class, variant -> ENTITY_TYPES.register("dugout_canoe/" + variant.getName(),
+    public static final Map<BoatVariant, RegistryObject<EntityType<CanoeEntity>>> CANOES = Helpers.mapOfKeys(
+            BoatVariant.class, variant -> ENTITY_TYPES.register("dugout_canoe/" + variant.getName(),
                     () -> EntityType.Builder.of(CanoeEntity::new, MobCategory.MISC).sized(1.125F, 0.625F)
                             .build(new ResourceLocation(Firmaciv.MOD_ID,
                                     "dugout_canoe/" + variant.getName()).toString())));
-    public static final Map<BoatVariant, RegistryObject<EntityType<RowboatEntity>>> ROWBOATS =
-            Helpers.mapOfKeys(BoatVariant.class, variant -> ENTITY_TYPES.register("rowboat/" + variant.getName(),
+
+    public static final Map<BoatVariant, RegistryObject<EntityType<RowboatEntity>>> ROWBOATS = Helpers.mapOfKeys(
+            BoatVariant.class, variant -> ENTITY_TYPES.register("rowboat/" + variant.getName(),
                     () -> EntityType.Builder.of(RowboatEntity::new, MobCategory.MISC).sized(1.875F, 0.625F)
                             .build(new ResourceLocation(Firmaciv.MOD_ID, "rowboat/" + variant.getName()).toString())));
 
@@ -44,6 +48,7 @@ public final class FirmacivEntities {
                     () -> EntityType.Builder.of(SloopEntity::new, MobCategory.MISC)
                             .sized(3F, 0.75F).setTrackingRange(LARGE_VEHICLE_TRACKING).fireImmune()
                             .build(new ResourceLocation(Firmaciv.MOD_ID, "sloop/" + variant.getName()).toString())));
+
     public static final RegistryObject<EntityType<KayakEntity>> KAYAK_ENTITY = ENTITY_TYPES.register("kayak",
             () -> EntityType.Builder.of(KayakEntity::new, MobCategory.MISC).sized(0.79F, 0.625F)
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "kayak").toString()));
@@ -51,17 +56,16 @@ public final class FirmacivEntities {
     public static final RegistryObject<EntityType<EmptyCompartmentEntity>> EMPTY_COMPARTMENT_ENTITY = ENTITY_TYPES.register(
             "compartment_empty",
             () -> EntityType.Builder.of(EmptyCompartmentEntity::new, MobCategory.MISC).sized(0.6F, 0.7F).noSummon()
-                    .build(new ResourceLocation(Firmaciv.MOD_ID, "compartment_empty").toString()));
+                    .build(new ResourceLocation(MOD_ID, "compartment_empty").toString()));
 
     public static final RegistryObject<EntityType<ChestCompartmentEntity>> CHEST_COMPARTMENT_ENTITY = ENTITY_TYPES.register(
             "compartment_chest",
             () -> EntityType.Builder.of(ChestCompartmentEntity::new, MobCategory.MISC).sized(0.6F, 0.7F).noSummon()
-                    .build(new ResourceLocation(Firmaciv.MOD_ID, "compartment_chest").toString()));
+                    .build(new ResourceLocation(MOD_ID, "compartment_chest").toString()));
 
-    public static final RegistryObject<EntityType<WorkbenchCompartmentEntity>> WORKBENCH_COMPARTMENT_ENTITY = ENTITY_TYPES.register(
+    public static final RegistryObject<EntityType<WorkbenchCompartmentEntity>> WORKBENCH_COMPARTMENT_ENTITY = register(
             "compartment_workbench",
-            () -> EntityType.Builder.of(WorkbenchCompartmentEntity::new, MobCategory.MISC).sized(0.6F, 0.7F).noSummon()
-                    .build(new ResourceLocation(Firmaciv.MOD_ID, "compartment_workbench").toString()));
+            EntityType.Builder.of(WorkbenchCompartmentEntity::new, MobCategory.MISC).sized(0.6F, 0.7F).noSummon());
 
     public static final RegistryObject<EntityType<AnvilCompartmentEntity>> ANVIL_COMPARTMENT_ENTITY = ENTITY_TYPES.register(
             "compartment_anvil",
@@ -69,18 +73,18 @@ public final class FirmacivEntities {
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "compartment_anvil").toString()));
 
     public static final RegistryObject<EntityType<BoatVehiclePart>> BOAT_VEHICLE_PART = ENTITY_TYPES.register(
-            "vehicle_part_boat",
-            () -> EntityType.Builder.of(BoatVehiclePart::new, MobCategory.MISC).sized(0.00F, 0.00F).setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
+            "vehicle_part_boat", () -> EntityType.Builder.of(BoatVehiclePart::new, MobCategory.MISC).sized(0, 0)
+                    .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_part_boat").toString()));
 
     public static final RegistryObject<EntityType<VehicleCleatEntity>> VEHICLE_CLEAT_ENTITY = ENTITY_TYPES.register(
-            "vehicle_cleat",
-            () -> EntityType.Builder.of(VehicleCleatEntity::new, MobCategory.MISC).sized(0.4F, 0.2F).setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
+            "vehicle_cleat", () -> EntityType.Builder.of(VehicleCleatEntity::new, MobCategory.MISC).sized(0.4F, 0.2F)
+                    .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_cleat").toString()));
 
     public static final RegistryObject<EntityType<VehicleCollisionEntity>> VEHICLE_COLLISION_ENTITY = ENTITY_TYPES.register(
             "vehicle_collider",
-            () -> EntityType.Builder.of(VehicleCollisionEntity::new, MobCategory.MISC).sized(1F, 1F).noSummon()
+            () -> EntityType.Builder.of(VehicleCollisionEntity::new, MobCategory.MISC).sized(1, 1).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_collider").toString()));
 
     public static final RegistryObject<EntityType<SailSwitchEntity>> SAIL_SWITCH_ENTITY = ENTITY_TYPES.register(
@@ -88,33 +92,43 @@ public final class FirmacivEntities {
             () -> EntityType.Builder.of(SailSwitchEntity::new, MobCategory.MISC).sized(0.8F, 0.8F).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_switch_sail").toString()));
 
-    public static final RegistryObject<EntityType<AnchorEntity>> ANCHOR_ENTITY = ENTITY_TYPES.register(
-            "vehicle_anchor",
-            () -> EntityType.Builder.of(AnchorEntity::new, MobCategory.MISC).sized(1.00F, 1.00F).clientTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
+    public static final RegistryObject<EntityType<AnchorEntity>> ANCHOR_ENTITY = ENTITY_TYPES.register("vehicle_anchor",
+            () -> EntityType.Builder.of(AnchorEntity::new, MobCategory.MISC).sized(1, 1)
+                    .clientTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_anchor").toString()));
 
     public static final RegistryObject<EntityType<WindlassSwitchEntity>> WINDLASS_SWITCH_ENTITY = ENTITY_TYPES.register(
             "vehicle_switch_windlass",
-            () -> EntityType.Builder.of(WindlassSwitchEntity::new, MobCategory.MISC).sized(0.5F, 0.5F).setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
+            () -> EntityType.Builder.of(WindlassSwitchEntity::new, MobCategory.MISC).sized(0.5F, 0.5F)
+                    .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_switch_windlass").toString()));
 
     public static final RegistryObject<EntityType<CannonballEntity>> CANNONBALL_ENTITY = ENTITY_TYPES.register(
             "cannonball",
-            () -> EntityType.Builder.of(CannonballEntity::new, MobCategory.MISC).sized(0.5F, 0.5F).setTrackingRange(32).clientTrackingRange(32).noSummon()
+            () -> EntityType.Builder.of(CannonballEntity::new, MobCategory.MISC).sized(0.5F, 0.5F).setTrackingRange(32)
+                    .clientTrackingRange(32).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "cannonball").toString()));
 
-    public static final RegistryObject<EntityType<CannonEntity>> CANNON_ENTITY = ENTITY_TYPES.register(
-            "cannon",
+    public static final RegistryObject<EntityType<CannonEntity>> CANNON_ENTITY = ENTITY_TYPES.register("cannon",
             () -> EntityType.Builder.of(CannonEntity::new, MobCategory.MISC).sized(0.8F, 0.8F)
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "cannon").toString()));
 
-    public static final RegistryObject<EntityType<MastEntity>> MAST_ENTITY = ENTITY_TYPES.register(
-            "vehicle_mast",
-            () -> EntityType.Builder.of(MastEntity::new, MobCategory.MISC).sized(0.3F, 8F).setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
+    public static final RegistryObject<EntityType<MastEntity>> MAST_ENTITY = ENTITY_TYPES.register("vehicle_mast",
+            () -> EntityType.Builder.of(MastEntity::new, MobCategory.MISC).sized(0.3F, 8F)
+                    .setTrackingRange(VEHICLE_HELPER_TRACKING).noSummon()
                     .build(new ResourceLocation(Firmaciv.MOD_ID, "vehicle_mast").toString()));
 
-    public static void register(IEventBus eventBus) {
-        ENTITY_TYPES.register(eventBus);
+    private static <E extends Entity> RegistryObject<EntityType<E>> register(final String name,
+            final EntityType.Builder<E> builder) {
+        return register(name, builder, true);
     }
 
+    private static <E extends Entity> RegistryObject<EntityType<E>> register(final String name,
+            final EntityType.Builder<E> builder, final boolean serialize) {
+        final String id = name.toLowerCase(Locale.ROOT);
+        return ENTITY_TYPES.register(id, () -> {
+            if (!serialize) builder.noSave();
+            return builder.build(MOD_ID + ":" + id);
+        });
+    }
 }
---
+++ src/main/java/com/alekiponi/firmaciv/common/entity/vehiclehelper/CompartmentType.java
@@ -0,0 +1,69 @@
+package com.alekiponi.firmaciv.common.entity.vehiclehelper;
+
+import com.alekiponi.firmaciv.common.entity.FirmacivEntities;
+import com.alekiponi.firmaciv.common.entity.vehiclehelper.compartment.AbstractCompartmentEntity;
+import com.alekiponi.firmaciv.common.entity.vehiclehelper.compartment.WorkbenchCompartmentEntity;
+import com.alekiponi.firmaciv.util.FirmacivTags;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public class CompartmentType<T extends AbstractCompartmentEntity> {
+    private static final List<CompartmentType<? extends AbstractCompartmentEntity>> COMPARTMENT_TYPES = new ArrayList<>();
+
+    public static final CompartmentType<WorkbenchCompartmentEntity> WORKBENCH = register(
+            FirmacivEntities.WORKBENCH_COMPARTMENT_ENTITY.get(), WorkbenchCompartmentEntity::new,
+            itemStack -> itemStack.is(FirmacivTags.Items.WORKBENCHES));
+
+    private final EntityType<T> entityType;
+    private final CompartmentFactory<T> factory;
+    private final Predicate<ItemStack> predicate;
+
+    private CompartmentType(final EntityType<T> entityType, final CompartmentFactory<T> factory,
+            final Predicate<ItemStack> predicate) {
+        this.entityType = entityType;
+        this.factory = factory;
+        this.predicate = predicate;
+    }
+
+    /**
+     * Registers a Compartment Type. These are used to dynamically find a compartment for an arbitrary item stack
+     *
+     * @param factory   The entity's factory
+     * @param predicate The predicate for checking when an item stack is applicable
+     */
+    public static <T extends AbstractCompartmentEntity> CompartmentType<T> register(final EntityType<T> entityType,
+            final CompartmentFactory<T> factory, Predicate<ItemStack> predicate) {
+        final CompartmentType<T> type = new CompartmentType<>(entityType, factory, predicate);
+        COMPARTMENT_TYPES.add(type);
+        return type;
+    }
+
+    /**
+     * Gets an applicable compartment for an item stack
+     *
+     * @param itemStack The item stack
+     * @return An applicable compartment for the given item stack. Returns the first compartment found,
+     * this means registry order can effect which is chosen
+     */
+    public static Optional<CompartmentType<?>> fromStack(final ItemStack itemStack) {
+        for (final CompartmentType<? extends AbstractCompartmentEntity> compartmentType : COMPARTMENT_TYPES) {
+            if (compartmentType.predicate.test(itemStack)) return Optional.of(compartmentType);
+        }
+
+        return Optional.empty();
+    }
+
+    public T create(final Level level, final ItemStack itemStack) {
+        return this.factory.create(this.entityType, level, itemStack);
+    }
+
+    public interface CompartmentFactory<T extends AbstractCompartmentEntity> {
+        T create(final EntityType<T> entityType, final Level level, final ItemStack itemStack);
+    }
+}
