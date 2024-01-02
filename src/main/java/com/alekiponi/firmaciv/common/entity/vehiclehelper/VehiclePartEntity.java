2307b39097b4d3a5e81a51ec33c6b8b01c8302ad
HyperDashPony <hyperdashpony@gmail.com>
2024-01-02 07:44:40 -0800

    rework vehicle parts adding their respective passengers

--- src/main/java/com/alekiponi/firmaciv/common/entity/vehiclehelper/VehiclePartEntity.java
+++ src/main/java/com/alekiponi/firmaciv/common/entity/vehiclehelper/VehiclePartEntity.java
@@ -27,7 +27,7 @@ public class VehiclePartEntity extends AbstractInvisibleHelper {
     public void tick() {
 
         // Most of vehiclepart's ticking actions should only be on the server
-        if(!this.level().isClientSide()) {
+        if (!this.level().isClientSide()) {
             if (!this.isPassenger()) {
                 selfDestructTicks++;
                 if (selfDestructTicks++ >= 5) {
@@ -52,119 +52,30 @@ public class VehiclePartEntity extends AbstractInvisibleHelper {
             }
 
             // Try not to be empty
-            if (this.getPassengers().isEmpty()) {
-                boolean shouldAddCleatInstead = false;
-                boolean shouldAddColliderInstead = false;
-                boolean shouldAddSailSwitchInstead = false;
-                boolean shouldAddWindlassInstead = false;
+            if (this.getPassengers().isEmpty() && vehicle.getPassengers().size() == vehicle.getMaxPassengers()) {
+                boolean alreadyHasPassenger = false;
                 // Try adding a cleat
-                if (vehicle.getPassengers().size() == vehicle.getMaxPassengers()) {
-                    for (int i : vehicle.getCleatIndices()) {
-                        if (vehicle.getPassengers().get(i).is(this) && !vehicle.getPassengers().get(i).isVehicle()) {
-
-                            shouldAddCleatInstead = true;
-
-                            final VehicleCleatEntity cleat = FirmacivEntities.VEHICLE_CLEAT_ENTITY.get()
-                                    .create(this.level());
-                            assert cleat != null;
-                            cleat.setPos(this.getX(), this.getY(), this.getZ());
-                            cleat.setYRot(this.getVehicle().getYRot());
-                            if (!cleat.startRiding(this)) {
-                                Firmaciv.LOGGER.error("New Cleat: {} unable to ride Vehicle Part: {}", cleat, this);
-                            }
-                            this.level().addFreshEntity(cleat);
-                            break;
-
-
-                        }
-                    }
-                }
+                alreadyHasPassenger = tickAddCleat(vehicle);;
 
                 //Try adding a collider
-                if(!shouldAddCleatInstead){
-                    if (vehicle.getPassengers().size() == vehicle.getMaxPassengers()) {
-                        for (int i : vehicle.getColliderIndices()) {
-                            if (vehicle.getPassengers().get(i).is(this) && !vehicle.getPassengers().get(i).isVehicle()) {
-
-                                shouldAddColliderInstead = true;
-
-                                final VehicleCollisionEntity collider = FirmacivEntities.VEHICLE_COLLISION_ENTITY.get()
-                                        .create(this.level());
-                                assert collider != null;
-                                collider.setPos(this.getX(), this.getY(), this.getZ());
-                                if (!collider.startRiding(this)) {
-                                    Firmaciv.LOGGER.error("New Collider: {} unable to ride Vehicle Part: {}", collider, this);
-                                }
-                                this.level().addFreshEntity(collider);
-                                break;
-
-                            }
-                        }
-                    }
+                if (!alreadyHasPassenger) {
+                    alreadyHasPassenger = tickAddCollider(vehicle);
                 }
 
                 // Try adding a switch
-                if(!shouldAddCleatInstead && !shouldAddColliderInstead && this.getVehicle() instanceof AbstractFirmacivBoatEntity boatEntity){
-                    if (vehicle.getPassengers().size() == vehicle.getMaxPassengers()) {
-                        for (int i : boatEntity.getSailSwitchIndices()) {
-                            if (vehicle.getPassengers().get(i).is(this) && !vehicle.getPassengers().get(i).isVehicle()) {
-
-                                shouldAddSailSwitchInstead = true;
-
-                                final AbstractSwitchEntity switchEntity = FirmacivEntities.SAIL_SWITCH_ENTITY.get()
-                                        .create(this.level());
-                                assert switchEntity != null;
-                                switchEntity.setPos(this.getX(), this.getY(), this.getZ());
-                                if (!switchEntity.startRiding(this)) {
-                                    Firmaciv.LOGGER.error("New Switch: {} unable to ride Vehicle Part: {}", switchEntity, this);
-                                }
-                                this.level().addFreshEntity(switchEntity);
-                                break;
-
-                            }
-                        }
-                    }
+                if (!alreadyHasPassenger && this.getVehicle() instanceof AbstractFirmacivBoatEntity boatEntity) {
+                    alreadyHasPassenger = tickAddSailSwitch(boatEntity);
                 }
 
                 // Try adding a Windlass
-                if(!shouldAddCleatInstead && !shouldAddColliderInstead && !shouldAddSailSwitchInstead && this.getVehicle() instanceof AbstractFirmacivBoatEntity boatEntity){
-                    if (boatEntity.getPassengers().size() == boatEntity.getMaxPassengers()) {
-                        for (int i : boatEntity.getWindlassIndices()) {
-                            if (boatEntity.getPassengers().get(i).is(this) && !boatEntity.getPassengers().get(i).isVehicle()) {
-
-                                shouldAddWindlassInstead = true;
-
-                                final WindlassSwitchEntity windlass = FirmacivEntities.WINDLASS_SWITCH_ENTITY.get()
-                                        .create(this.level());
-                                assert windlass != null;
-                                windlass.setPos(this.getX(), this.getY(), this.getZ());
-                                if (!windlass.startRiding(this)) {
-                                    Firmaciv.LOGGER.error("New Windlass: {} unable to ride Vehicle Part: {}", windlass, this);
-                                }
-                                this.level().addFreshEntity(windlass);
-                                break;
-
-                            }
-                        }
-                    }
+                if (!alreadyHasPassenger && this.getVehicle() instanceof AbstractFirmacivBoatEntity boatEntity) {
+                    tickAddWindlass(boatEntity);
+                    alreadyHasPassenger = tickAddWindlass(boatEntity);
                 }
 
                 // Try adding a compartment
-                if (vehicle.getPassengers().size() == vehicle.getMaxPassengers()) {
-                    if (!shouldAddCleatInstead && !shouldAddColliderInstead && !shouldAddSailSwitchInstead && !shouldAddWindlassInstead) {
-                        final EmptyCompartmentEntity newCompartment = FirmacivEntities.EMPTY_COMPARTMENT_ENTITY.get()
-                                .create(this.level());
-
-                        if (newCompartment != null) {
-                            newCompartment.setYRot(this.getVehicle().getYRot() + this.getCompartmentRotation());
-                            newCompartment.setPos(this.getX(), this.getY(), this.getZ());
-                            if (!newCompartment.startRiding(this)) {
-                                Firmaciv.LOGGER.error("New Compartment: {} unable to ride Vehicle Part: {}", newCompartment,
-                                        this);
-                            }
-                            this.level().addFreshEntity(newCompartment);
-                        }
-                    }
+                if (!alreadyHasPassenger) {
+                    alreadyHasPassenger = tickAddCompartment(vehicle);
                 }
 
 
@@ -191,6 +102,99 @@ public class VehiclePartEntity extends AbstractInvisibleHelper {
         }
     }
 
+    protected boolean tickAddCleat(AbstractVehicle vehicle) {
+        for (int i : vehicle.getCleatIndices()) {
+            if (vehicle.getPassengers().get(i).is(this) && !vehicle.getPassengers().get(i).isVehicle()) {
+
+                final VehicleCleatEntity cleat = FirmacivEntities.VEHICLE_CLEAT_ENTITY.get()
+                        .create(this.level());
+                assert cleat != null;
+                cleat.setPos(this.getX(), this.getY(), this.getZ());
+                cleat.setYRot(this.getVehicle().getYRot());
+                if (!cleat.startRiding(this)) {
+                    Firmaciv.LOGGER.error("New Cleat: {} unable to ride Vehicle Part: {}", cleat, this);
+                }
+                this.level().addFreshEntity(cleat);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected boolean tickAddCollider(AbstractVehicle vehicle) {
+        for (int i : vehicle.getColliderIndices()) {
+            if (vehicle.getPassengers().get(i).is(this) && !vehicle.getPassengers().get(i).isVehicle()) {
+
+                final VehicleCollisionEntity collider = FirmacivEntities.VEHICLE_COLLISION_ENTITY.get()
+                        .create(this.level());
+                assert collider != null;
+                collider.setPos(this.getX(), this.getY(), this.getZ());
+                if (!collider.startRiding(this)) {
+                    Firmaciv.LOGGER.error("New Collider: {} unable to ride Vehicle Part: {}", collider, this);
+                }
+                this.level().addFreshEntity(collider);
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    protected boolean tickAddSailSwitch(AbstractFirmacivBoatEntity boat) {
+        for (int i : boat.getSailSwitchIndices()) {
+            if (boat.getPassengers().get(i).is(this) && !boat.getPassengers().get(i).isVehicle()) {
+
+                final AbstractSwitchEntity switchEntity = FirmacivEntities.SAIL_SWITCH_ENTITY.get()
+                        .create(this.level());
+                assert switchEntity != null;
+                switchEntity.setPos(this.getX(), this.getY(), this.getZ());
+                if (!switchEntity.startRiding(this)) {
+                    Firmaciv.LOGGER.error("New Switch: {} unable to ride Vehicle Part: {}", switchEntity, this);
+                }
+                this.level().addFreshEntity(switchEntity);
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    protected boolean tickAddWindlass(AbstractFirmacivBoatEntity boat) {
+        for (int i : boat.getWindlassIndices()) {
+            if (boat.getPassengers().get(i).is(this) && !boat.getPassengers().get(i).isVehicle()) {
+
+
+
+                final WindlassSwitchEntity windlass = FirmacivEntities.WINDLASS_SWITCH_ENTITY.get()
+                        .create(this.level());
+                assert windlass != null;
+                windlass.setPos(this.getX(), this.getY(), this.getZ());
+                if (!windlass.startRiding(this)) {
+                    Firmaciv.LOGGER.error("New Windlass: {} unable to ride Vehicle Part: {}", windlass, this);
+                }
+                this.level().addFreshEntity(windlass);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected boolean tickAddCompartment(AbstractVehicle vehicle) {
+        final EmptyCompartmentEntity newCompartment = FirmacivEntities.EMPTY_COMPARTMENT_ENTITY.get()
+                .create(this.level());
+
+        if (newCompartment != null) {
+            newCompartment.setYRot(this.getVehicle().getYRot() + this.getCompartmentRotation());
+            newCompartment.setPos(this.getX(), this.getY(), this.getZ());
+            if (!newCompartment.startRiding(this)) {
+                Firmaciv.LOGGER.error("New Compartment: {} unable to ride Vehicle Part: {}", newCompartment,
+                        this);
+            }
+            this.level().addFreshEntity(newCompartment);
+            return true;
+        }
+        return false;
+    }
 
 
     @Override
@@ -198,11 +202,11 @@ public class VehiclePartEntity extends AbstractInvisibleHelper {
         this.entityData.define(DATA_ID_COMPARTMENT_ROTATION, 0f);
     }
 
-    public void setCompartmentRotation(float rotation){
+    public void setCompartmentRotation(float rotation) {
         this.entityData.set(DATA_ID_COMPARTMENT_ROTATION, rotation);
     }
 
-    public float getCompartmentRotation(){
+    public float getCompartmentRotation() {
         return this.entityData.get(DATA_ID_COMPARTMENT_ROTATION);
     }
 
