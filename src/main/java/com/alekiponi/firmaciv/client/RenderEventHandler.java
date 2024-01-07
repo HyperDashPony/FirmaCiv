7bb58595c5def6bf5dfdafce6927e30c756af5d3
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-01-07 15:04:35 -0800

    Make CompartmentRenderer an abstract class to make it easy to have custom renderers. Normal empty/block compartments use BlockCompartmentRenderer which renders the displayBlockstate

--- src/main/java/com/alekiponi/firmaciv/client/RenderEventHandler.java
+++ src/main/java/com/alekiponi/firmaciv/client/RenderEventHandler.java
@@ -10,10 +10,7 @@ import com.alekiponi.firmaciv.client.render.entity.vehicle.CanoeRenderer;
 import com.alekiponi.firmaciv.client.render.entity.vehicle.KayakRenderer;
 import com.alekiponi.firmaciv.client.render.entity.vehicle.RowboatRenderer;
 import com.alekiponi.firmaciv.client.render.entity.vehicle.SloopRenderer;
-import com.alekiponi.firmaciv.client.render.entity.vehicle.vehiclehelper.AnchorRenderer;
-import com.alekiponi.firmaciv.client.render.entity.vehicle.vehiclehelper.CompartmentRenderer;
-import com.alekiponi.firmaciv.client.render.entity.vehicle.vehiclehelper.InvisibleHelperRenderer;
-import com.alekiponi.firmaciv.client.render.entity.vehicle.vehiclehelper.VehicleCleatRenderer;
+import com.alekiponi.firmaciv.client.render.entity.vehicle.vehiclehelper.*;
 import com.alekiponi.firmaciv.common.entity.FirmacivEntities;
 import com.alekiponi.firmaciv.util.BoatVariant;
 import net.minecraftforge.api.distmarker.Dist;
@@ -39,12 +36,14 @@ public final class RenderEventHandler {
         }
         event.registerEntityRenderer(FirmacivEntities.KAYAK_ENTITY.get(), KayakRenderer::new);
 
-        event.registerEntityRenderer(FirmacivEntities.EMPTY_COMPARTMENT_ENTITY.get(), CompartmentRenderer::new);
-        event.registerEntityRenderer(FirmacivEntities.TFC_CHEST_COMPARTMENT_ENTITY.get(), CompartmentRenderer::new);
-        event.registerEntityRenderer(FirmacivEntities.BARREL_COMPARTMENT_ENTITY.get(), CompartmentRenderer::new);
+        event.registerEntityRenderer(FirmacivEntities.EMPTY_COMPARTMENT_ENTITY.get(), BlockCompartmentRenderer::new);
+        event.registerEntityRenderer(FirmacivEntities.TFC_CHEST_COMPARTMENT_ENTITY.get(),
+                BlockCompartmentRenderer::new);
+        event.registerEntityRenderer(FirmacivEntities.BARREL_COMPARTMENT_ENTITY.get(), BlockCompartmentRenderer::new);
         event.registerEntityRenderer(FirmacivEntities.CHEST_COMPARTMENT_ENTITY.get(), ChestCompartmentRenderer::new);
-        event.registerEntityRenderer(FirmacivEntities.WORKBENCH_COMPARTMENT_ENTITY.get(), CompartmentRenderer::new);
-        event.registerEntityRenderer(FirmacivEntities.ANVIL_COMPARTMENT_ENTITY.get(), CompartmentRenderer::new);
+        event.registerEntityRenderer(FirmacivEntities.WORKBENCH_COMPARTMENT_ENTITY.get(),
+                BlockCompartmentRenderer::new);
+        event.registerEntityRenderer(FirmacivEntities.ANVIL_COMPARTMENT_ENTITY.get(), BlockCompartmentRenderer::new);
 
         event.registerEntityRenderer(FirmacivEntities.BOAT_VEHICLE_PART.get(), InvisibleHelperRenderer::new);
         event.registerEntityRenderer(FirmacivEntities.SAIL_SWITCH_ENTITY.get(), InvisibleHelperRenderer::new);
--- src/main/java/com/alekiponi/firmaciv/client/render/entity/vehicle/vehiclehelper/CompartmentRenderer.java
+++ src/main/java/com/alekiponi/firmaciv/client/render/entity/vehicle/vehiclehelper/CompartmentRenderer.java
@@ -6,29 +6,23 @@ import com.alekiponi.firmaciv.common.entity.vehiclehelper.compartment.AbstractCo
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.texture.MissingTextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class CompartmentRenderer extends EntityRenderer<AbstractCompartmentEntity> {
-    private final BlockRenderDispatcher blockRenderer;
-
-    public CompartmentRenderer(EntityRendererProvider.Context pContext/*, ModelLayerLocation pLayer*/) {
-        super(pContext);
-        this.shadowRadius = 0.0F;
-        this.blockRenderer = pContext.getBlockRenderDispatcher();
+public abstract class CompartmentRenderer<CompartmentType extends AbstractCompartmentEntity> extends EntityRenderer<CompartmentType> {
+    public CompartmentRenderer(EntityRendererProvider.Context context) {
+        super(context);
+        this.shadowRadius = 0;
     }
 
-    public void render(final AbstractCompartmentEntity compartmentEntity, final float entityYaw,
-            final float partialTicks, final PoseStack poseStack, final MultiBufferSource bufferSource,
-            final int packedLight) {
+    @Override
+    public void render(final CompartmentType compartmentEntity, final float entityYaw, final float partialTicks,
+            final PoseStack poseStack, final MultiBufferSource bufferSource, final int packedLight) {
         super.render(compartmentEntity, entityYaw, partialTicks, poseStack, bufferSource, packedLight);
 
         if (compartmentEntity.tickCount < 2) {
@@ -52,25 +46,20 @@ public class CompartmentRenderer extends EntityRenderer<AbstractCompartmentEntit
         }
 
         poseStack.translate(-0.5F, 0, -0.5F);
-        {
-            if (compartmentEntity.getDisplayBlockState().getRenderShape() != RenderShape.INVISIBLE) {
-                this.renderCompartmentContents(compartmentEntity, partialTicks,
-                        compartmentEntity.getDisplayBlockState(), poseStack, bufferSource, packedLight);
-            }
-        }
+
+        this.renderCompartmentContents(compartmentEntity, partialTicks, poseStack, bufferSource, packedLight);
 
         poseStack.popPose();
     }
 
-    @Override
-    public ResourceLocation getTextureLocation(final AbstractCompartmentEntity compartmentEntity) {
-        return null;
-    }
+    /**
+     * Render the compartment contents. This is pre-scaled, rotated and translated for ease of use
+     */
+    protected abstract void renderCompartmentContents(final CompartmentType compartmentEntity, final float partialTicks,
+            final PoseStack poseStack, final MultiBufferSource bufferSource, final int packedLight);
 
-    protected void renderCompartmentContents(final AbstractCompartmentEntity compartmentEntity,
-            final float partialTicks, final BlockState blockState, final PoseStack poseStack,
-            final MultiBufferSource bufferSource, final int packedLight) {
-        this.blockRenderer.renderSingleBlock(blockState, poseStack, bufferSource, packedLight,
-                OverlayTexture.NO_OVERLAY);
+    @Override
+    public ResourceLocation getTextureLocation(final CompartmentType compartmentEntity) {
+        return MissingTextureAtlasSprite.getLocation();
     }
 }
